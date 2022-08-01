1febcbe7df6adcfb87ffb4917dd0def3685d2626
Festus Adesiju <festorium10@gmail.com>
2022-08-01 16:07:11 +0100

    good

--- adminPHIS/roles.py
+++ adminPHIS/roles.py
@@ -1,16 +1,39 @@
+from aiohttp.web_response import json_response
 from django.http import HttpResponse
 from django.shortcuts import redirect
+
+import jwt
+from rest_framework import exceptions
+
+from phis_admin.adminPHIS.models import PhisUser
+
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
 
 
 def authenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return view_func(request, *args, **kwargs)
+
+        request.user = None
+        jwt_token = request.headers.get('authorization', None)
+        if jwt_token:
+            try:
+                payload = jwt.decode(jwt_token, JWT_SECRET,
+                                     algorithms=[JWT_ALGORITHM])
+            except (jwt.DecodeError, jwt.ExpiredSignatureError):
+                return json_response({'message': 'Token is invalid'},
+                                     status=400)
+
+            request.user = PhisUser.objects.get(id=payload['user_id'])
+            # if request.user.is_authenticated:
+            if request.user:
+                return view_func(request, *args, **kwargs)
+            else:
+                return HttpResponse('You are not authorized to view this page')
         else:
-            return HttpResponse('You are not authorized to view this page')
+            raise exceptions.AuthenticationFailed("Authorization header is not present")
 
     return wrapper_func
-
 
 # header_data = curl GET(jwt.get_unverified_header(token))
 # try:
@@ -21,7 +44,6 @@ def authenticated_user(view_func):
 #     )
 # except ExpiredSignatureError as error:
 #     print(f'Unable to decode token, error: {error}')
-
 
 
 # def allowed_users(allowed_roles=[]):
