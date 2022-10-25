63d351d155d9d014ba54521de2249951b1826d87
Akande Victor <40690359+Akandeav@users.noreply.github.com>
2022-10-25 21:08:18 +0100

    Fixed follower query

--- adminPHIS/views.py
+++ adminPHIS/views.py
@@ -499,7 +499,7 @@ def getAuthor(request, pk):
         author = PhisUser.objects.filter(auth_user_id=pk).first()
         application = AuthorApplication.objects.filter(email=author.email, status="A").first()
         if author is not None and application is not None and user is not None:
-            follower = AuthorApplication.objects.filter(email=author.email, followers=user).first()
+            follower = AuthorApplication.objects.get(email=author.email, followers__id=user.id)
             if follower is not None:
                 isFollower = True
             else:
@@ -569,9 +569,9 @@ def followAuthor(request, format=None):
     phis_user_id = request.payload['id']
     phis_user = PhisUser.objects.filter(auth_user_id=phis_user_id).first()
     author = PhisUser.objects.filter(auth_user_id=author_id).first()
-    follower = AuthorApplication.objects.filter(author_email=author.email, followers=phis_user).first()
-    if follower is not None and author.user_role != "P":
-        follower.followers.add(phis_user)
+    followers = AuthorApplication.objects.get(email=author.email, followers__id=phis_user_id)
+    if followers is not None and author.user_role != "P":
+        followers.followers.add(phis_user)
         follower.number_followers += 1
         follower.save()
         response.data = {
@@ -594,7 +594,7 @@ def unfollowAuthor(request, format=None):
     phis_user_id = request.payload['id']
     phis_user = PhisUser.objects.filter(auth_user_id=phis_user_id).first()
     author = PhisUser.objects.filter(auth_user_id=author_id).first()
-    followers = AuthorApplication.objects.filter(author_email=author.email, followers=phis_user).first()
+    followers = AuthorApplication.objects.get(email=author.email, followers__id=phis_user_id)
     if followers is not None and author.user_role != "P":
         followers.followers.remove(phis_user)
         followers.number_followers -= 1
